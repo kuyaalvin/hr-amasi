b0492a24ce4e3402998ba0508fe53dca4eac8e12
morbidCode <morbidcode666@gmail.com>
2017-10-21 21:52:49 +0800

    added server-side validations for employees

--- app/models/Employee.php
+++ app/models/Employee.php
@@ -20,4 +20,43 @@ class Employee extends GlobalModel
         return $this->belongsTo(Project::class, 'project_id');
     }
     
+    public function validate($data)
+    {
+        $rules = [
+            'id_number'=>['required', $this->uniqueRule('employees', 'id_number', $this->id_number), 'digits_between:1,10'],
+            'biomentric_id'=>['nullable', $this->uniqueRule('employees', 'biometric_id', $this->biometric_id), 'digits_between:1,255'],
+            'account_number'=>['required', $this->uniqueRule('employees', 'account_number', $this->account_number), 'digits_between:1,25'],
+            'last_name'=>['required', 'string', 'max:50'],
+            'middle_name'=>['required', 'string', 'max:100'],
+            'first_name'=>['required', 'string', 'max:50'],
+            'city_address'=>['required', 'string', 'max:255'],
+            'provincial_address'=>['nullable', 'string', 'max:255'],
+            'birthdate'=>['required', 'bail', 'date_format:' . $this->dateFormat, 'before:today'],
+            'civil_status'=>['required', 'in:Single,Married'],
+            'number_of_dependencies'=>['nullable', 'integer', 'min:0', 'max:127'],
+            'citizenship'=>['required', 'string', 'max:30'],
+            'religion'=>['nullable', 'string', 'max:30'],
+            'gender'=>['required', 'in:Male,Female'],
+            'sss_id'=>['nullable', 'digits_between:1,20'],
+            'tin_no'=>['nullable', 'digits_between:1,20'],
+            'phil_health_mp'=>['nullable', 'digits_between:1,20'],
+            'payroll_type'=>['required', 'in:Weekly,Monthly'],
+            'date_started'=>['required', 'date_format:' . $this->dateFormat],
+            'agency'=>['required', 'boolean'],
+            'regular'=>['required', 'boolean'],
+            'position_id'=>['nullable', 'exists:positions'],
+            'project_id'=>['nullable', 'exists:positions'],
+        ];
+        
+        $messages = [
+            'birthdate.before' => 'The birthdate must be a date before today.',
+            'birthdate.date_format'=>'The birthdate does not match the format YYYY-MM-DD.',
+            'date_started.date_format'=>'The date started does not match the format YYYY-MM-DD.',
+        ];
+        
+        $validator = validator($data, $rules, $messages);
+        $this->errors = $validator->errors();
+        return $validator->passes();
+    }
+    
 }
