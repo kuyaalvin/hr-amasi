8768dd4dc33e668fc349bed6c4faa6d7abd461e3
morbidCode <morbidcode666@gmail.com>
2017-10-21 15:41:20 +0800

    configure date and time

--- app/models/Employee.php
+++ app/models/Employee.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Employee extends Model
 {
     protected $primaryKey = 'employee_id';
     public $timestamps = false;
+    protected $dates = ['birthdate', 'date_started'];
+    protected $dateFormat = 'Y-m-d';
     protected $guarded = ['employee_id', 'active'];
 
     public function position()
--- app/models/Project.php
+++ app/models/Project.php
@@ -9,7 +9,9 @@ class Project extends Model
     protected $primaryKey = 'project_id';
     public $timestamps = false;
     protected $guarded = ['project_id', 'active'];
-    
+protected $dates = ['time_in', 'time_out'];
+protected $dateFormat = 'H:i:s';
+
     public function employees()
     {
         return $this->hasMany(Employee::class);
