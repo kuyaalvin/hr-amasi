8f1c6972db17720d9e501a28cb780770c0a9e7c6
morbidCode <morbidcode666@gmail.com>
2017-10-26 15:58:13 +0800

    reordered migrations
    Warning: migrations before this commit might not work. Run php artisan db:fresh to sink.

--- database/migrations/2017_10_26_115335_CreateEvents.php
+++ database/migrations/2017_10_26_115335_CreateEvents.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSetActiveEmployeesEvent extends Migration
+class CreateEvents extends Migration
 {
     /**
      * Run the migrations.
