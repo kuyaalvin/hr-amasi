3dc1655d10c93691515bc4971e839aaf51d44890
morbidCode <morbidcode666@gmail.com>
2017-10-28 00:41:52 +0800

    handle named routes

--- app/http/controllers/GlobalController.php
+++ app/http/controllers/GlobalController.php
@@ -10,13 +10,31 @@ class GlobalController extends Controller
 
 protected function sendResponse(Request $request, GlobalModel $model, bool $status, string $redirect = null, string $statusMessage = null)
 {
+    $router = app('router');
+    $isNamedRoute = $router->has($redirect);
     $errors = $model->errors();
     $response;
-// if ($request->ajax() || $request->wantsJson())
-// {
-    $response = response()->json(($status ? ['redirect'=>$redirect] : $errors), ($status ? 200 : 422));
-// }
-// $response = $status ? redirect($redirect) : back()->withErrors($errors)->withInput();
+if ($request->ajax() || $request->wantsJson())
+{
+$responseData;
+$statusCode;
+if ($status)
+{
+    $responseData = ['redirect'=>($isNamedRoute) ? route($redirect) : url($redirect)];
+    $statusCode = 200;
+} else {
+    $responseData = $errors;
+$statusCode = 422;
+}
+    $response = response()->json($responseData, $statusCode);
+} else {
+    if ($status)
+    {
+        $response = $isNamedRoute ? redirect()->route($redirect) : redirect($redirect);
+    } else {
+        $response = back()->withErrors($errors)->withInput();
+    }
+}
 if (isset($statusMessage))
 {
     $request->session()->flash('status', $statusMessage);
