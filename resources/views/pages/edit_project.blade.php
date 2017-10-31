45b84d282e88af37347b37a34d67f2650de6b1ca
morbidCode <morbidcode666@gmail.com>
2017-10-31 15:20:58 +0800

    temporarily removed divs

--- resources/views/pages/edit_project.blade.php
+++ resources/views/pages/edit_project.blade.php
@@ -10,24 +10,14 @@
 	<form id="editProjectForm" action="{{ url('projects/' . $project->project_id) }}" method="post">
 {{ csrf_field() }}
 {{ method_field('patch') }}
-		<div class="form-row">
-			<div class="form-group col-md-4">
 				<label for="projectName">Project Name</label>
 				<input type="text" class="form-control" name="name" placeholder="edit project here" value="{{ old('name', $project->name) }}">
-			</div>
-			<div class="form-group col-md-4">
 				<label for="projectAddress">Project Address</label>
 				<input type="text" class="form-control" name="address" placeholder="edit address here" value="{{ old('address', $project->address) }}">
-			</div>
-			<div class="form-group col-md-4">
 				<label for="projectName">Time In</label>
 				<input type="text" class="form-control" name="time_in" placeholder="edit time in here" value="{{ old('time_in', $project->time_in) }}">
-			</div>
-			<div class="form-group col-md-4">
 				<label for="time_out">Time Out</label>
 				<input type="text" class="form-control" name="time_out" placeholder="edit time out here" value="{{ old('time_out', $project->time_out) }}">
-			</div>
-		</div>
 		<input type="submit" value="Submit">
 	</form>
 @endsection
