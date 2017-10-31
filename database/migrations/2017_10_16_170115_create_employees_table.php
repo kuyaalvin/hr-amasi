2ce59e81b67c129ddcb973cd14f590408246065f
morbidCode <morbidcode666@gmail.com>
2017-11-01 00:08:03 +0800

    update employees table

--- database/migrations/2017_10_16_170115_create_employees_table.php
+++ database/migrations/2017_10_16_170115_create_employees_table.php
@@ -15,23 +15,27 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->increments('employee_id');
-            $table->char('id_number', 10)->unique();
-            $table->char('biometric_id', 255)->nullable()->unique();
+            $table->char('id_number', 8)->unique();
+            $table->char('biometric_id', 4)->nullable()->unique();
             $table->char('account_number', 25)->unique();
             $table->string('last_name', 50);
             $table->string('first_name', 100);
             $table->string('middle_name', 50);
             $table->string('city_address', 255);
             $table->string('provincial_address', 255)->nullable();
+$table->char('telephone_number', 9);
+$table->char('cellphone_number1', 13);
+$table->char('cellphone_number2', 13);
             $table->date('birthdate');
             $table->enum('civil_status', ['Single', 'Married']);
             $table->unsignedTinyInteger('number_of_dependencies')->nullable();
             $table->string('citizenship', 30);
             $table->string('religion', 30)->nullable();
             $table->enum('gender', ['Male', 'Female']);
-            $table->char('sss_id', 20)->nullable();
-            $table->char('tin_no', 20)->nullable();
-            $table->char('phil_health_mp', 20)->nullable();
+            $table->char('sss_id', 12)->nullable();
+            $table->char('phic_id', 14)->nullable();
+            $table->char('hdmf_id', 14)->nullable();
+            $table->char('tin_id', 15)->nullable();
             $table->enum('payroll_type', ['Weekly', 'Monthly']);
             $table->date('date_started');
             $table->boolean('agency');
