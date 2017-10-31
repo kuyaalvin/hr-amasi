6b55b0d532216072b8629f7f157d273a27e2e246
morbidCode <morbidcode666@gmail.com>
2017-11-01 00:12:59 +0800

    added divs again

--- resources/views/pages/add_project.blade.php
+++ resources/views/pages/add_project.blade.php
@@ -9,14 +9,24 @@
 
 	<form id="createProjectForm" action="{{ url('projects') }}" method="post">
 {{ csrf_field() }}
+		<div class="form-row">
+			<div class="form-group col-md-4">
 				<label for="projectName">Project Name</label>
 				<input type="text" class="form-control" name="name" placeholder="add project name here" value="{{ old('name') }}"/>
+</div>
+			<div class="form-group col-md-4">
 				<label for="projectAddress">Project Address</label>
 				<input type="text" class="form-control" name="address" placeholder="add project address here" value="{{ old('address') }}"/>
+</div>
+			<div class="form-group col-md-4">
 				<label for="timeIn">Time In</label>
 				<input type="text" class="form-control" name="time_in" placeholder="add time in here" value="{{ old('time_in') }}"/>
+</div>
+			<div class="form-group col-md-4">
 				<label for="timeOut">Time Out</label>
 				<input type="text" class="form-control" name="time_out" placeholder="add time out here" value="{{ old('time_out') }}"/>
+</div>
+</div>
 		<input type="submit" value="Submit"/>
 	</form>
 
