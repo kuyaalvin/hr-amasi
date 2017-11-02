e2a27f995b936e2c2e4d095c6a9c5fc0b31117b5
kuyaalvin <josephalvindavid@gmail.com>
2017-11-02 20:39:34 +0800

    Added errors folder. Added 404, 500 blades

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+	<h1>400: no page found</h1>
+	
+
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+	<h1>500: internal error</h1>
+
+
+@endsection
