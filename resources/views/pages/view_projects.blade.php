d0aae7a55b3a55afcb2ac4fc8e93d3590d7d1c4d
morbidCode <morbidcode666@gmail.com>
2017-12-01 17:39:20 +0800

    fix: bootstrap enabled on projects

--- resources/views/pages/view_projects.blade.php
+++ resources/views/pages/view_projects.blade.php
@@ -42,7 +42,8 @@ filter += '<option value="'+index+'">'+columnValue+'</option>';
 filter += "</option";
 
 var table = $('.table').DataTable({
-dom: 'l<"#filter">rtip',
+//dom: 'l<"#filter">rtip',
+dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'<\"#filter\">>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
     	serverSide: true,
         processing: true,
         ajax: '',
