26e0c9ff675793bad199386201e637df031bae1a
kuyaalvin <josephalvindavid@gmail.com>
2017-12-07 15:23:02 +0800

    Added Timepicker for Time in/out

--- resources/views/pages/add_project.blade.php
+++ resources/views/pages/add_project.blade.php
@@ -4,34 +4,76 @@
 
 @include('layouts.sidebaremployee')
 
-<h1>add project</h1>
+
+<div class="container">
+<h1>Add Project</h1>
 @if ($errors->any())
 @foreach ($errors->all() as $error)
-<label for="errorMessage">{{ $error }}</label>
+<div class="alert alert-danger" for="errorMessage" role="alert">
+{{ $error }}
+</div>
 @endforeach
 @endif
 
 	<form action="{{ url('projects') }}" method="post">
 		{{ csrf_field() }}
-		<div class="form-row">
-			<div class="form-group col-md-4">
+			<div class="row form-group col-md-12">
 				<label for="projectName">Project Name</label>
 				<input type="text" class="form-control" name="name" placeholder="add project name here" value="{{ old('name') }}">
 			</div>
-			<div class="form-group col-md-4">
+			<div class="row form-group col-md-12">
 				<label for="projectAddress">Project Address</label>
-				<input type="text" class="form-control" name="address" placeholder="add project address here" value="{{ old('address') }}">
+				<textarea type="text" class="form-control" name="address" placeholder="add project address here" value="{{ old('address') }}" rows="3"></textarea>
 			</div>
-			<div class="form-group col-md-4">
+
+			<div class="row">
+			<div class="form-group col-md-6">
 				<label for="timeIn">Time In</label>
-				<input  class="form-control" name="time_in" placeholder="add time in here" value="{{ old('time_in') }}">
+				<input  class="form-control" name="time_in" id="timepicker" placeholder="add time in here" value="{{ old('time_in') }}">
 			</div>
-			<div class="form-group col-md-4">
+			<div class="form-group col-md-6">
 				<label for="timeOut">Time Out</label>
-				<input class="form-control" name="time_out" placeholder="add time out here" value="{{ old('time_out') }}">
+				<input class="form-control" name="time_out" id="timepickerout" placeholder="add time out here" value="{{ old('time_out') }}">
 			</div>
-		</div>
-		<input type="submit" value="Submit">
+			</div>
+			<div class="form-group">
+				<input class="btn btn-primary" type="submit" value="Submit">
+			</div>
+
+		
 	</form>
+</div>
+
+@push('scripts')
+<script>
+
+
+	$('#timepicker').timepicker({
+    timeFormat: 'HH:mm:ss',
+    interval: 30,
+    minTime: '0',
+    maxTime: '23:00',
+    defaultTime: '0',
+    startTime: '00:00',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true
+	});
+
+	$('#timepickerout').timepicker({
+    timeFormat: 'HH:mm:ss',
+    interval: 30,
+    minTime: '0',
+    maxTime: '23:00',
+    defaultTime: '0',
+    startTime: '00:00',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true
+	});
+
+</script>
+@endpush
 
 @endsection
+
