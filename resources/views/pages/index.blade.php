265ddb92d6f9d2ef67cd7d21172c542c63943b8f
kuyaalvin <josephalvindavid@gmail.com>
2017-12-08 16:42:08 +0800

    Revamp Login Page design

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,43 +1,73 @@
 @extends('layouts.applogin')
 
 @section('contentlogin')
-
-
-
 		
-		<div class="container h-100">
-		<blockquote class="blockquote text-center">
-		  <p class="mb-0">Welcome to HR AMASI</p>
-		 <small class="text-muted">please sign-in</small>
-		</blockquote>	
+	<section class="login-block">
+    <div class="container">
+	<div class="row">
+		<div class="col-md-4 login-sec">
+			<h3 class="text-center">Welcome to AMASI HRM SYSTEM</h3>
+		    <h2 class="text-center">Login Now</h2>
+	<form class="login-form" action="{{ url('login') }}" method="post">
+		{{ csrf_field() }}
+  <div class="form-group">
 
-			<div class="row h-100 justify-content-center align-items-center">
+    <label for="username" class="text-uppercase">Username</label>
+    <input type="text" id="username" class="form-control" name="username" value="{{ old('username') }}" placeholder=""/>
+    
+  </div>
+  <div class="form-group">
+    <label for="password" class="text-uppercase">Password</label>
+    <input type="password" id="password" class="form-control" name="password" placeholder="">
+  </div>
+  
+  	
 
-
-			<form action="{{ url('login') }}" method="post">
-					{{ csrf_field() }}
-				<div class="form-group">
-					<label for="username">User Name</label>
-					<input id="username" class="form-control" name="username" value="{{ old('username') }}"/>
-				</div>
-				<div class="form-group">
-					<label for="password">Password</label>
-					<input id="password" class="form-control" type="password" name="password"/>
-				</div>
-				<div class="form-group">
-					<input type="submit" class="btn btn-primary" value="Login">
-				</div>
-			</form>
-			@if ($errors->any())
+    <div class="form-group">
+    
+    @if ($errors->any())
+	<div class="alert alert-danger" role="alert">
+	<label for="errorMessage">{{ $errors->all()[0] }}</label>
+	</div>
+	@endif
+  </div>
+  <button type="submit" class="btn btn-login float-right">Submit</button>
+</form>
 
 
 		</div>
-
-		<div class="row h-100 alert alert-danger justify-content-center" role="alert">
-			<label for="errorMessage">{{ $errors->all()[0] }}</label>
-			</div>
-			@endif
+		<div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  </ol>
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>Bugs? Errors?</h2>
+            <p>Please contact system admin @ *insert phone number here*</p>
+        </div>	
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>Birthdays for the Month of *Insert Current Month Here*</h2>
+            <p>*List here*</p>
+        </div>	
+    </div>
+    </div>
+    
+            </div>	   
+		    
 		</div>
+	</div>
+</div>
+</section>
 
 
 
