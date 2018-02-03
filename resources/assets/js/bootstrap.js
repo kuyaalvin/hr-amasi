22b2941b4b690c381d4aacab55ad5ad3d456650f
morbidCode <morbidcode666@gmail.com>
2018-02-03 22:19:15 +0800

    added datatables select

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -14,5 +14,6 @@ require('datatables.net-bs4');
 require('datatables.net-buttons-bs4');
 require('datatables.net-buttons/js/buttons.colVis');
 require('datatables.net-buttons/js/buttons.html5');
+require('datatables.net-select')();
 require('bootstrap-timepicker');
 } catch (e) {}
