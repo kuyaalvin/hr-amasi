7e63dbae5783a27f29b480ae374ff5a8a7098180
morbidCode <morbidcode666@gmail.com>
2018-02-24 02:12:15 +0800

    removed datatables select

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -14,7 +14,6 @@ require('datatables.net-bs4');
 require('datatables.net-buttons-bs4');
 require('datatables.net-buttons/js/buttons.colVis');
 require('datatables.net-buttons/js/buttons.html5');
-require('datatables.net-select')();
 require('bootstrap-timepicker');
 require('inputmask/dist/jquery.inputmask.bundle.js');
 } catch (e) {}
