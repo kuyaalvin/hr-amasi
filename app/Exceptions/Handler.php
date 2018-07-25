cb5415445345703511d622fa8d2f6824a0f650b8
morbidCode <morbidcode666@gmail.com>
2018-07-26 01:36:31 +0800

    restored error displays to default

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,11 +49,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-if ($exception instanceof  HttpException)
-{
+// if ($exception instanceof  HttpException)
+// {
     return parent::render($request, $exception);
-}
-return response()->view('errors/500', [], 500);
+// }
+// return response()->view('errors/500', [], 500);
     }
 
 }
