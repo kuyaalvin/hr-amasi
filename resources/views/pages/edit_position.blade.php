ab3a7a412109b3c1521d9cf85da73b048e187b5d
morbidCode <morbidcode666@gmail.com>
2018-09-01 10:37:43 +0800

    removed positions level on project add/edit pages

--- resources/views/pages/edit_position.blade.php
+++ resources/views/pages/edit_position.blade.php
@@ -31,35 +31,6 @@
 
 		<div class="form-row">
 			<div class="form-group col-md-4">
-				<label for="positionName">Position Level: *</label>
-				<select class="form-control" name="level" id="gender0">
-					<option value="">Select Position Level</option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="">4</option>
-					<option value="">5</option>
-					<option value="">6</option>
-					<option value="">7</option>
-					<option value="">8</option>
-					<option value="">9</option>
-					<option value="">10</option>
-					<option value="">11</option>
-					<option value="">12</option>
-					<option value="">13</option>
-					<option value="">14</option>
-					<option value="">15</option>
-					<option value="">16</option>
-					<option value="">17</option>
-					<option value="">18</option>
-					<option value="">19</option>
-					<option value="">20</option>
-			    </select>
-			</div>
-		</div>
-
-		<div class="form-row">
-			<div class="form-group col-md-4">
 				<label for="positionName">Department: *</label>
 				<select class="form-control" name="department_id" id="gender0">
 					<option value="">Select Department</option>
