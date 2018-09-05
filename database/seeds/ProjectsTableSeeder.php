b166bbbcc304200a7d2090ac80bfd4a126717b9e
morbidCode <morbidcode666@gmail.com>
2018-09-06 03:49:50 +0800

    set seconds to 0 on project seeds

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -22,10 +22,10 @@ for ($i = 0; $i < 10; $i++)
 {
         $timeInHour = rand(0, 23);
         $timeInMinutes = rand(0, 59);
-        $timeInSeconds = rand(0, 59);
+        $timeInSeconds = 0;
         $timeOutHour = rand($timeInHour+1, 24);
         $timeOutMinutes = rand(0, 59);
-        $timeOutSeconds = rand(0, 59);
+        $timeOutSeconds = 0;
         
         do {
             $name = str_random(rand(2, 5));
