fe46f2c11afb6dee7b654580c4a6be2df727d6ab
morbidCode <morbidcode666@gmail.com>
2018-09-29 01:17:30 +0800

    fix: migration classes cannot use snake case

--- database/migrations/2018_09_29_010319_create_employees_projects_table.php
+++ database/migrations/2018_09_29_010319_create_employees_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Builder;
 
-class create_employees_projects_table extends Migration
+class CreateEmployeesProjectsTable extends Migration
 {
     private $schema;
     
